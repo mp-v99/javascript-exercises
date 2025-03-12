2aae75d5eef10a2c68c6de5289ec095a8849e5d7
mpv <193361506+mp-v99@users.noreply.github.com>
2025-03-12 14:44:46 -0600

    Solve exercise #3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,11 @@
-const reverseString = function() {
+const reverseString = function(userInput) {
+    const toArray = userInput.split("");
+    toArray.reverse();
+    userInput = toArray.join('');
 
-};
+    return userInput
+    
+};  
 
 // Do not edit below this line
 module.exports = reverseString;
