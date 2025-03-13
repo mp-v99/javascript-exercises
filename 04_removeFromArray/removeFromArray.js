aeada9003c6a239056e84e1f9d1dbd12ef94b11b
mpv <193361506+mp-v99@users.noreply.github.com>
2025-03-13 14:25:19 -0600

    Solve exercise #4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,26 @@
-const removeFromArray = function() {
+const removeFromArray = function(userArray, ...elementsToRemove) {
+
+    const filterArray = userArray.filter(element => !elementsToRemove.includes(element))
+    return filterArray;
+
 };
+
+  
+        /* for(const userElement of userArray) {
+           
+        }
+        return newArray; */
+   
+
+//pseudocode:
+
+/* 
+
+    DECLARE filterArray
+
+    RUN a filter LOOP to check each value in userArray
+    RETURN only those values that don't match a value in elementsToRemove
+*/
 
 // Do not edit below this line
 module.exports = removeFromArray;
