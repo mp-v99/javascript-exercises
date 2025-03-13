16113600185f7e07d2966a78a88e0d6d7cf21f8f
mpv <193361506+mp-v99@users.noreply.github.com>
2025-03-13 16:20:19 -0600

    Solve exercise #6

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,5 @@
-const leapYears = function() {
-
+const leapYears = function(inputYear) {
+    return(inputYear % 4 === 0 && inputYear % 100 !== 0 || inputYear % 400 === 0)
 };
 
 // Do not edit below this line
