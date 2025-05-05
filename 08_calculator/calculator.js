061f35adb073c32f04d6d808f7c23b07c302fcb5
mpv <193361506+mp-v99@users.noreply.github.com>
2025-05-05 17:50:37 -0600

    solve exercises

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,30 +1,46 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  return arr.reduce((acc, item) => {
+    return acc + item;
+  }, 0)
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((acc, item) => {
+    return acc * item;
+  })
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+  if (b === 0) return 1;
+  let power = a;
+	for (let i = 1; i < b; i++) {
+    power *= a
+  };
+  return power;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  if (a === 0) return 1;
+  let b = [];
+  for (let i = 1; i <= a; i++) {
+    b.unshift(i)
+  }
+  return b.reduce((acc, item) => {
+    return acc * item
+  });
 };
 
 // Do not edit below this line
 module.exports = {
-  add,
+  add,  
   subtract,
   sum,
   multiply,
--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,21 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    let originalStr = str
+                        .replace(/[^a-z0-9]/gi, '')
+                        .toLowerCase()
+    let arr = originalStr
+                        .split('')
+                        .reverse()
+                        .join('');
+    if (arr == originalStr) {
+        return true;
+    }
+    else {
+        return false;
+    }
 };
+
+
+
 
 // Do not edit below this line
 module.exports = palindromes;
