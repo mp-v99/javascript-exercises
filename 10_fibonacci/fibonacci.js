de6b1762e9daa4859175d6725add6e1d40d9b7d6
mpv <193361506+mp-v99@users.noreply.github.com>
2025-05-05 19:26:53 -0600

    solve exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,18 @@
-const fibonacci = function() {
+const fibonacci = function(index) {
 
+    let arr = [0]; 
+    let newValue = 1;
+
+    if (index < 0) {
+        return 'OOPS'
+    }
+
+    for (let i = 0; i < index; i++) {
+        arr.push(newValue)
+        newValue = newValue + arr[i]
+    }
+    return arr[index];
 };
-
+//  1, 1, 2, 3
 // Do not edit below this line
 module.exports = fibonacci;
