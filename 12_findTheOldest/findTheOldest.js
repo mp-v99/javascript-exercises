51ec7fd201f5c419c1f7ef8c3a21ebac2c6180c3
mpv <193361506+mp-v99@users.noreply.github.com>
2025-05-05 20:39:07 -0600

    Solve exercise #12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,13 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) {
+    let currentYear = (new Date()).getFullYear();
+    arr.forEach((item) => {
+        item.age = item.yearOfDeath - item.yearOfBirth;
+        if (!item.yearOfDeath) {
+        item.age = currentYear - item.yearOfBirth;
+        }
+    })
+    arr.sort((a, b) => b.age - a.age);
+    return arr[0]
 };
 
 // Do not edit below this line
